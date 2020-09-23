3c366218086f0cee178d92accdcf2f103c44b7d1
Vitali Grecciani <artlogistles@gmail.com>
2020-09-23 18:38:19 +0300

    First commit

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable { 
+    public static void main(String[] args) {
+        byte cpuSize = 4;
+        short year = 2020;
+        int specification = 173793 ;
+        long frequency = 2437000;
+        char model = 'A';
+        float weight = 2.62245f;
+        double screenSize = 1.1166666666666667d;
+        boolean bool = true;
+        System.out.println(cpuSize);
+        System.out.println(year);
+        System.out.println(specification);
+        System.out.println(frequency);
+        System.out.println(model);
+        System.out.println(weight);
+        System.out.println(screenSize);
+        System.out.println(bool);
+    }
+}

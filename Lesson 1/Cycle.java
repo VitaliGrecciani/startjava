664fc954a5efd3243dbc16545366dab273752f9e
Vitali Grecciani <artlogistles@gmail.com>
2020-10-06 10:21:39 +0300

    bugs fixed

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -4,20 +4,20 @@ public class Cycle {
 			System.out.println(i);
 		}
 
-		int  value = 6; 
-		while ( value >= -6) {
-			System.out.println("Result" +  value);
-			 value -= 2;
+		int counter = 6; 
+		while (counter >= -6) {
+			System.out.println("Result" + counter);
+			counter -= 2;
 		}
 
-		 value = 10;
+		counter = 10;
 		int summ = 0;
 		do {
-			if (value % 2 != 0) {
-				summ += value;
+			if (counter % 2 != 0) {
+				summ += counter;
 			}
-			value++;
-		} while(value != 20);
+			counter++;
+		} while(counter != 20);
 		System.out.println("Sum = " + summ);
 	}
 }

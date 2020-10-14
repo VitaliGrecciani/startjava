633efb835374e5d638677d629170b54a2a160542
Vitali Grecciani <artlogistles@gmail.com>
2020-10-14 12:34:16 +0300

    Lesson 1/Game added

---
+++ Lesson 1/MyFirstGame.java
@@ -0,0 +1,17 @@
+public class MyFirstGame {
+		public static void main(String[] args) {
+			int correctNumber = 13;
+			int inputNumber = 50;
+
+			while(inputNumber != correctNumber) {
+				if (inputNumber < correctNumber) {
+					System.out.println("Введеное вами число меньше того, что загадал компьютер");
+					inputNumber++;
+				} else if(inputNumber > correctNumber) {
+					System.out.println("Введеное вами число больше того, что загадал компьютер");
+					inputNumber--;
+			}
+		}	
+		System.out.println("Вы угадали " + correctNumber);	
+	}
+}
---
+++ Lesson 1/MyNewGame.java
@@ -0,0 +1,18 @@
+public class MyNewGame {
+
+    public static void main(String[] args) {
+        int correctNumber = 13;
+        int inputNumber = 50;
+        
+        do {
+            if (inputNumber < correctNumber) {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                inputNumber++;
+            } else if (inputNumber > correctNumber) {
+                System.out.println("Введенное вами число больше того, что загадал компьютер");
+                inputNumber--;
+            }
+        } while (inputNumber != correctNumber);
+        System.out.println("Вы угадали " + correctNumber);
+    }
+}

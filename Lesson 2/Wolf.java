4eb66c6281443078ec31b76d51b00dbb21656f6b
Vitali Grecciani <artlogistles@gmail.com>
2020-10-23 16:20:45 +0300

    Wolf added

---
+++ Lesson 2/Wolf.java
@@ -0,0 +1,23 @@
+public class Wolf {
+    String name;
+    String sex;
+    int age;
+    int weight;
+    String color;
+    
+    boolean hunt() {
+        return true;
+    }
+    
+    void go() {
+    }
+    
+    void sit() {
+    }
+    
+    void howl() {
+    }
+    
+    void run() {
+    }
+}

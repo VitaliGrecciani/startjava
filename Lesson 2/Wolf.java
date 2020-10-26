68e58a30290a16b1dc5ca56c361aa24c1dc7799e
Vitali Grecciani <artlogistles@gmail.com>
2020-10-26 19:52:57 +0200

    Wolf added

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -1,23 +1,29 @@
 public class Wolf {
-    String name;
-    String sex;
-    int age;
-    int weight;
-    String color;
-    
-    boolean hunt() {
-        return true;
-    }
-    
-    void go() {
-    }
-    
-    void sit() {
-    }
-    
-    void howl() {
-    }
-    
-    void run() {
+        String nickName = "Rex";
+        String sex = "Male";
+        int age = 7;
+        int weight = 42;
+        String color = "black";
+        
+        boolean hunt() {
+            System.out.println("Hare hunting");
+            return true;
+        
+        }
+        void go() {
+            System.out.println("Max speed 7 km/h");
+        
+        }
+        void sit() {
+            System.out.println("When tired");
+        
+        }
+        void howl() {
+            System.out.println("howling at the moon");
+        
+        }
+        void run() {
+            System.out.println("Max speed 50 km/h");
     }
 }
+
---
+++ Lesson 2/WolfTest.java
@@ -0,0 +1,17 @@
+public class WolfTest {
+
+    public static void main(String[] args) {
+        Wolf wolfFirst = new Wolf();
+            System.out.println("nickName = " + wolfFirst.nickName);
+            System.out.println("sex = " + wolfFirst.sex);
+            System.out.println("age = " + wolfFirst.age);
+            System.out.println("weight = " + wolfFirst.weight);
+            System.out.println("color = " + wolfFirst.color);
+            System.out.println(wolfFirst.hunt());
+            wolfFirst.go();
+            wolfFirst.sit();
+            wolfFirst.howl();
+            wolfFirst.run();
+            System.out.println("");
+    }
+}

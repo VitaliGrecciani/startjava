84663fa2ce89af6a422b9e44864fc72b333c8ace
Vitali Grecciani <artlogistles@gmail.com>
2020-10-27 13:20:20 +0200

    Fixed

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -4,24 +4,24 @@ public class Wolf {
         int age = 7;
         int weight = 42;
         String color = "black";
-        
+
         boolean hunt() {
             System.out.println("Hare hunting");
             return true;
-        
         }
+
         void go() {
             System.out.println("Max speed 7 km/h");
-        
         }
+
         void sit() {
             System.out.println("When tired");
-        
         }
+
         void howl() {
             System.out.println("howling at the moon");
-        
         }
+
         void run() {
             System.out.println("Max speed 50 km/h");
     }
--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -2,16 +2,16 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfFirst = new Wolf();
-            System.out.println("nickName = " + wolfFirst.nickName);
-            System.out.println("sex = " + wolfFirst.sex);
-            System.out.println("age = " + wolfFirst.age);
-            System.out.println("weight = " + wolfFirst.weight);
-            System.out.println("color = " + wolfFirst.color);
-            System.out.println(wolfFirst.hunt());
-            wolfFirst.go();
-            wolfFirst.sit();
-            wolfFirst.howl();
-            wolfFirst.run();
-            System.out.println("");
+        System.out.println("nickName = " + wolfFirst.nickName);
+        System.out.println("sex = " + wolfFirst.sex);
+        System.out.println("age = " + wolfFirst.age);
+        System.out.println("weight = " + wolfFirst.weight);
+        System.out.println("color = " + wolfFirst.color);
+        System.out.println(wolfFirst.hunt());
+        wolfFirst.go();
+        wolfFirst.sit();
+        wolfFirst.howl();
+        wolfFirst.run();
+        System.out.println("");
     }
 }

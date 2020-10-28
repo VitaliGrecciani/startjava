c0baeabb455695f4c81eeba9838d2ee23188a597
Vitali Grecciani <artlogistles@gmail.com>
2020-10-28 20:12:26 +0200

    getter/setter added

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -1,29 +1,73 @@
 public class Wolf {
-        String nickName = "Rex";
-        String sex = "Male";
-        int age = 7;
-        int weight = 42;
-        String color = "black";
+        private String nickName = "Rex";
+        private String sex = "Male";
+        private String colour = "black";
+        private int age = 7;
+        private int weight = 42;
+        
+        public String getNickName() {
+            return nickName;
+        }
+        
+        public void setNickName(String nickName) {
+            this.nickName = nickName;
+        }
+        
+        public String getSex() {
+            return sex;
+        }
+        
+        public void setSex(String sex) {
+            this.sex = sex;
+        }
+        
+        public String getColour() {
+            return colour;
+        }
+        
+        public void setColour(String colour) {
+            this.colour = colour;
+        }
 
-        boolean hunt() {
+        public int getAge() {
+            return age;
+        }
+        
+        public void setAge(int age) {
+            if (age > 8) {
+                System.out.println("Некорректный возраст");
+            }
+            else {
+                this.age = age;
+            }
+        }
+        
+        public int getWeight() {
+            return weight;
+        }
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+        
+        public boolean hunt() {
             System.out.println("Hare hunting");
             return true;
         }
 
-        void go() {
+        public void go() {
             System.out.println("Max speed 7 km/h");
         }
 
-        void sit() {
+        public void sit() {
             System.out.println("When tired");
         }
 
-        void howl() {
+        public void howl() {
             System.out.println("howling at the moon");
         }
 
-        void run() {
+        public void run() {
             System.out.println("Max speed 50 km/h");
-    }
+        }
 }
 
--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -2,12 +2,19 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfFirst = new Wolf();
-        System.out.println("nickName = " + wolfFirst.nickName);
-        System.out.println("sex = " + wolfFirst.sex);
-        System.out.println("age = " + wolfFirst.age);
-        System.out.println("weight = " + wolfFirst.weight);
-        System.out.println("color = " + wolfFirst.color);
+        
+        wolfFirst.setNickName("Kex");
+        wolfFirst.setSex("female");
+        wolfFirst.setAge(5);
+        wolfFirst.setWeight(40);
+        wolfFirst.setColour("white");
+        System.out.println("nickName = " + wolfFirst.getNickName);
+        System.out.println("sex = " + wolfFirst.getSex);
+        System.out.println("age = " + wolfFirst.getAge);
+        System.out.println("weight = " + wolfFirst.getWeight);
+        System.out.println("colour = " + wolfFirst.getColour);
         System.out.println(wolfFirst.hunt());
+        
         wolfFirst.go();
         wolfFirst.sit();
         wolfFirst.howl();

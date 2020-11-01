5e8d4f0331c855207a94020e2d51ca025ae4c6b7
Vitali Grecciani <artlogistles@gmail.com>
2020-11-01 14:52:49 +0200

    поставил скобки

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -8,11 +8,11 @@ public class WolfTest {
         wolfFirst.setAge(5);
         wolfFirst.setWeight(40);
         wolfFirst.setColour("white");
-        System.out.println("nickName = " + wolfFirst.getNickName);
-        System.out.println("sex = " + wolfFirst.getSex);
-        System.out.println("age = " + wolfFirst.getAge);
-        System.out.println("weight = " + wolfFirst.getWeight);
-        System.out.println("colour = " + wolfFirst.getColour);
+        System.out.println("nickName = " + wolfFirst.getNickName());
+        System.out.println("sex = " + wolfFirst.getSex());
+        System.out.println("age = " + wolfFirst.getAge());
+        System.out.println("weight = " + wolfFirst.getWeight());
+        System.out.println("colour = " + wolfFirst.getColour());
         System.out.println(wolfFirst.hunt());
         
         wolfFirst.go();

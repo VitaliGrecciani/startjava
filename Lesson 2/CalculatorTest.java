44b68278a6518beceb3dc6acb626e09b7e5b2214
Vitali Grecciani <artlogistles@gmail.com>
2020-11-06 20:05:54 +0200

    Calculator-Lesson2 added

---
+++ Lesson 2/CalculatorTest.java
@@ -0,0 +1,5 @@
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator casio = new Calculator();
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Введите первое число:");

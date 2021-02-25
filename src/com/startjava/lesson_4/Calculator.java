206f84d6b0876c29e69e866f5410f9224c0ee43c
Vitali Grecciani <artlogistles@gmail.com>
2021-02-25 11:55:11 +0200

    Добавлен урок 4.

---
+++ src/com/startjava/lesson_4/Calculator.java
@@ -0,0 +1,49 @@
+package com.startjava.lesson_4;
+
+public class Calculator {
+    private int num1;
+    private int num2;
+    private char operation;
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(int num2) { this.num2 = num2;}
+
+    public void setOperation(char operation) {
+        this.operation = operation;
+    }
+
+    public void calculate() {
+        switch (operation) {
+            case '+':
+                System.out.println(num1 + "" + operation + "" + num2 + "=" + Math.addExact(num1, num2));
+                break;
+            case '-':
+                System.out.println(num1 + "" + operation + "" + num2 + "=" + Math.subtractExact(num1, num2));
+                break;
+            case '*':
+                System.out.println(num1 + "" + operation + "" + num2 + "=" + Math.multiplyExact(num1, num2));
+                break;
+            case '/':
+                System.out.println(num1 + "" + operation + "" + num2 + "=" + Math.floorDiv(num1, num2));
+                break;
+            case '^':
+                System.out.println(num1 + "" + operation + "" + num2 + "=" + (int) Math.pow(num1, num2));
+                break;
+            case '%':
+                System.out.println(num1 + "" + operation + "" + num2 + "=" + IEEEremainder(num1, num2));
+                break;
+            default:
+                System.out.println(" ");
+        }
+    }
+
+//    public void setNum1(int parseInt) {
+//  }
+
+//    public void setNum2(int parseInt) {
+//  }
+}
+
---
+++ src/com/startjava/lesson_4/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.startjava.lesson_4;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+class CalculatorTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите математическое выражение: ");
+        String mathExpression = scanner.nextLine();
+        String[] mathExpressionArray = mathExpression.split(" ");
+
+        Calculator casio = new Calculator();
+        casio.setNum1(parseInt(mathExpressionArray[0]));
+        casio.setOperation(mathExpressionArray[1].charAt(0));
+        casio.setNum2(parseInt(mathExpressionArray[2]));
+        casio.calculate();
+    }
+}

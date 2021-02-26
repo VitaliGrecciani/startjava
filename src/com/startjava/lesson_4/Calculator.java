121ef25e9e003f0927691294ce856a898c79231e
Vitali Grecciani <artlogistles@gmail.com>
2021-02-26 11:25:00 +0200

    1. Папкa .idea и файл startjava.iml -> .gitignore 2. Math.IEEEremainder

--- src/com/startjava/lesson_4/Calculator.java
+++ src/com/startjava/lesson_4/Calculator.java
@@ -6,44 +6,42 @@ public class Calculator {
     private char operation;
 
     public void setNum1(int num1) {
+
         this.num1 = num1;
     }
 
-    public void setNum2(int num2) { this.num2 = num2;}
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
 
     public void setOperation(char operation) {
+
         this.operation = operation;
     }
 
     public void calculate() {
         switch (operation) {
             case '+':
-                System.out.println(num1 + "" + operation + "" + num2 + "=" + Math.addExact(num1, num2));
+                System.out.println("Результат "+ num1 + operation  + num2 + "=" + Math.addExact(num1, num2));
                 break;
             case '-':
-                System.out.println(num1 + "" + operation + "" + num2 + "=" + Math.subtractExact(num1, num2));
+                System.out.println( "Результат " + num1  + operation + num2 + "=" + Math.subtractExact(num1, num2));
                 break;
             case '*':
-                System.out.println(num1 + "" + operation + "" + num2 + "=" + Math.multiplyExact(num1, num2));
+                System.out.println("Результат " +  num1 + operation + num2 + "=" + Math.multiplyExact(num1, num2));
                 break;
             case '/':
-                System.out.println(num1 + "" + operation + "" + num2 + "=" + Math.floorDiv(num1, num2));
+                System.out.println( "Результат " + num1 + operation + num2 + "=" + Math.floorDiv(num1, num2));
                 break;
             case '^':
-                System.out.println(num1 + "" + operation + "" + num2 + "=" + (int) Math.pow(num1, num2));
+                System.out.println( "Результат " + num1 + operation + num2 + "=" + (int) Math.pow(num1, num2));
                 break;
             case '%':
-                System.out.println(num1 + "" + operation + "" + num2 + "=" + IEEEremainder(num1, num2));
+                System.out.println("Результат " +  num1 + operation + num2 + "=" + Math.IEEEremainder(num1, num2));
                 break;
             default:
                 System.out.println(" ");
         }
     }
-
-//    public void setNum1(int parseInt) {
-//  }
-
-//    public void setNum2(int parseInt) {
-//  }
 }
 
